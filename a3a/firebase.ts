2679d9f9a2ff1b7fbbec656f6e3fa7e3ae2c4c0d
fghareeb <134479266+fghareeb@users.noreply.github.com>
2024-10-16 20:04:07 -0400

    Update firebase.ts

--- a3a/firebase.ts
+++ a3a/firebase.ts
@@ -5,13 +5,7 @@ import { getStorage, ref, getDownloadURL } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCvuXlcrum_JFkBg-PCFybxGhtxM1KvkRs",
-  authDomain: "a3a-test.firebaseapp.com",
-  projectId: "a3a-test",
-  storageBucket: "a3a-test.appspot.com",
-  messagingSenderId: "659802263062",
-  appId: "1:659802263062:web:3201d52d5d031dfdc7af37",
-  measurementId: "G-3HXSDFR6S1"
+
 };
 
 // Initialize Firebase
